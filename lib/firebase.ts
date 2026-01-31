7e7e8ad393cc79dabbec7cae6ca4c3b8fa2422af
Ahmed Alshatebi <ahmedalshatibi22@gmail.com>
2026-01-31 15:21:00 +0300

    firebase configuration to use environment variables for improved security

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDQObcQ2teVHcgfaCDGX3quowDTCXqQc68",
-  authDomain: "moviesite-179b2.firebaseapp.com",
-  projectId: "moviesite-179b2",
-  storageBucket: "moviesite-179b2.firebasestorage.app",
-  messagingSenderId: "876050580636",
-  appId: "1:876050580636:web:c7e151112d3a703caa9959",
-  measurementId: "G-JF4J3FF33Z"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
